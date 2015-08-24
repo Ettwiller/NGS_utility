b96b4e626d85862b0c185b70571b963abf4595e5
ettwiller <ettwiller@neb.com>
2015-08-24 11:04:52 -0400

    fixing error in invert_R1_in_sam.pl

--- invert_R1_in_sam.pl
+++ invert_R1_in_sam.pl
@@ -8,7 +8,7 @@ use Getopt::Long qw(GetOptions);
 
 #alterinatively the oneliner is : 
 #samtools view -h accepted_hits.bam | perl -ne 'if($_ !~/^\@/){ @tmp = split/\t/,$_; $flag = (split/\t/,$_)[1]; if($flag & 64){if ($flag & 16){$flag =$flag-16; $tmp[1]=$flag; $line =join("\t", @tmp); print "$line"} else {$flag =$flag+16; $tmp[1]=$flag; $line =join("\t", @tmp); print "$line"}} else {print "$_"}} else {print "$_"}' > test.sam
-
+my $sam;
 
 
 my $usage_sentence = "perl $0 --sam samfile.sam\nthis program reverse the R1 of a paired-end read. This program can be used upstream of bedtools multicov when counting a paired-end reads of a RNA-seq strand specific library. In this case the R1 and R2 will have both the same orientation";
@@ -21,7 +21,7 @@ if (!$sam) {die $usage_sentence;}
 
 
 
-my $sam = $ARGV[0];
+
 
 open (SAM, $sam) or die " can't open the sam file $sam\n";
 foreach my $line (<SAM>)
